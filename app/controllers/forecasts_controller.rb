b9a0b3fff0a4873972f846653758e8676128cacb
Raghu Betina <rbetina@github.com>
2015-01-14 09:59:28 -0600

    Weather forecast working with variable route segment

---
+++ app/controllers/forecasts_controller.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+
+class ForecastsController < ApplicationController
+  def location
+
+    # params = {"address"=>"5807+S+Woodlawn+Ave"}
+
+    @url_safe_address = URI.encode(params[:address])
+
+    geocoding_api_url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}"
+
+    raw_results = open(geocoding_api_url).read
+
+    parsed_results = JSON.parse(raw_results)
+
+    @latitude = parsed_results["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_results["results"][0]["geometry"]["location"]["lng"]
+
+    forecast_api_url = "https://api.forecast.io/forecast/03cc290f7831236148648675effe2c52/#{@latitude},#{@longitude}"
+
+    raw_results = open(forecast_api_url).read
+
+    parsed_results = JSON.parse(raw_results)
+
+    @forecast_results = parsed_results
+  end
+end
